6dd05f7535218cdfbfc99cbb5fbca7092df572aa
hanseopark <hapark@cern.ch>
2022-09-12 20:50:26 +0200

    changes made on 2022-09-12T20:50:26

--- Script/pp/Jets/0_RunJets.sh
+++ Script/pp/Jets/0_RunJets.sh
@@ -6,7 +6,14 @@ echo "Jet analysis"
 #o2-analysistutorial-jet-analysis --aod-file ${DATA}/AO2D.root | o2-analysis-je-jet-finder -b | o2-analysis-event-selection -b | o2-analysis-timestamp -b | o2-analysis-trackselection -b | o2-analysis-trackextension -b | o2-analysis-je-jet-finder-hf -b
 
 # JET TUTORIAL for jet analysis
-o2-analysistutorial-jet-analysis --aod-file ${DATA}/AO2D.root | o2-analysis-je-jet-finder -b | o2-analysis-event-selection -b | o2-analysis-timestamp -b | o2-analysis-trackselection -b | o2-analysis-trackextension -b | o2-analysis-je-jet-skimmer -b
+#o2-analysistutorial-jet-analysis --aod-file ${DATA}/AO2D.root | o2-analysis-je-jet-finder -b | o2-analysis-event-selection -b | o2-analysis-timestamp -b | o2-analysis-trackselection -b | o2-analysis-trackextension -b | o2-analysis-je-jet-skimmer -b
 
 # HFJET
 #o2-analysis-hf-d0-candidate-selector --aod-file ${DATA}/AO2D.root | o2-analysis-pid-tof-full -b | o2-analysis-timestamp -b | o2-analysis-pid-tof-base | o2-analysis-pid-tpc-full | o2-analysis-multiplicity-table -b | o2-analysis-hf-track-index-skims-creator -b | o2-analysis-trackselection -b | o2-analysis-trackextension -b | o2-analysis-hf-candidate-creator-2prong -b | o2-analysis-je-jet-finder-hf -b
+
+# JET FINDER NORMEL
+o2-analysis-je-jet-finder --aod-file ${DATA}/AO2D.root | o2-analysis-timestamp -b | o2-analysis-event-selection -b | o2-analysis-multiplicity-table -b | o2-analysis-pid-tof-base -b | o2-analysis-pid-tof-full -b | o2-analysis-pid-tpc-full -b | o2-analysis-trackselection -b | o2-analysis-trackextension -b
+
+# HF JET FOR D0 MESON
+#o2-analysis-je-jet-substructure --aod-file ${DATA}/AO2D.root | o2-analysis-hf-d0-candidate-selector -b  | o2-analysis-pid-tof-full -b | o2-analysis-timestamp -b | o2-analysis-pid-tof-base | o2-analysis-pid-tpc-full | o2-analysis-multiplicity-table -b | o2-analysis-hf-track-index-skims-creator -b | o2-analysis-trackselection -b | o2-analysis-trackextension -b | o2-analysis-event-selection -b | o2-analysis-hf-candidate-creator-2prong -b | o2-analysis-je-jet-finder-hf -b | o2-analysis-je-jet-finder -b
+
